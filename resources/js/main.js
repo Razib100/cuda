ba2390120184f583e2d0a8bd87e2f8246fad74f9
Razib100 <razibhasan634@gmail.com>
2022-03-06 22:10:36 -0800

    first commmit

---
+++ resources/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    
+    //STICKY MENU
+    $(".js--services-ection").waypoint(function(direction){
+        if(direction == "down"){
+                $("nav").addClass("sticky")
+           }
+           else{
+                $("nav").removeClass("sticky");
+           }
+    });
+      
+    //ACTIVE LINK
+    $("nav ul li a").click(function(){
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+    
+    $("nav a img.logo").click(function(){
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+    
+    
+    //PORTFOLIO SECTION IMAGE FILTER
+    var mixer = mixitup('.container');
+    
+    //SMOOTH SCROLL FOR IE/SAFARI/EDGE
+    
+    $("a").on("click", function(event) {
+        if(this.hash !== ""){
+            event.preventDefault();
+            
+            var hash = this.hash;
+            
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800 , function(){
+                window.location.hash = hash;
+            }
+            });
+        }
+    });
+});
+
+
+//MOBILE MENU
+
+function openNav(){
+    document.getElementById("myNav").style.width= "100%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width= "0%";
+}
+
+
+
+
+
+
+
+
+
